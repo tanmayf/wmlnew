d1dc409431de5c2e7521dd13a776555cc35cd72a
tanmayf <freestff007700@gmail.com>
2025-03-24 14:41:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = "7654518078:AAF8IyfBs42uURw1bjjxvI9DF2r1c-NvXp4"
 TELEGRAM_API = 23904398
 TELEGRAM_HASH = "c55934364222dc3d4155320d2ced1359"
 OWNER_ID = 891959176
-UPSTREAM_REPO = "https://github.com/tanmayf/mlw-upstream"
-UPSTREAM_BRANCH = "main"
+UPSTREAM_REPO = "https://github.com/SilentDemonSD/WZML-X"
+UPSTREAM_BRANCH = "wzv3"
 DATABASE_URL = "mongodb+srv://orzipdrin:w3WCKeMMR8Qg12zQ@cluster0.7p8qh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 BASE_URL = "https://sff2-0948d85cacb5.herokuapp.com/"
