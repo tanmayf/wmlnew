34126b209132807b9be99be0ce9d53c53cbdf290
tanmayf <freestff007700@gmail.com>
2025-03-24 14:18:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-BOT_TOKEN = ""
-TELEGRAM_API = 0
-TELEGRAM_HASH = ""
-OWNER_ID = 0
+BOT_TOKEN = "7654518078:AAF8IyfBs42uURw1bjjxvI9DF2r1c-NvXp4"
+TELEGRAM_API = 23904398
+TELEGRAM_HASH = "c55934364222dc3d4155320d2ced1359"
+OWNER_ID = 891959176
 UPSTREAM_REPO = ""
 UPSTREAM_BRANCH = "wzv3"
-DATABASE_URL = ""
+DATABASE_URL = "mongodb+srv://errinnmar:PmCx7sH134W7iHZb@cluster0.vclm2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 BASE_URL = ""
